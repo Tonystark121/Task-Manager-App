13d05797475bd41c2fec4f494a66e7db09745d98
rajeev <rajeev.kumar.phe21@itbhu.ac.in>
2024-04-07 23:12:37 +0530

    add navbar and functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,91 @@ import Task from "./pages/task";
 import TaskDetails from "./pages/taskdetails";
 import Users from "./pages/users";
 import Team from "./pages/team";
-import {Toaster} from "sonner";
+import { Toaster } from "sonner";
+import Sidebar from "./components/sidebar";
+import Navbar from "./components/navbar";
+import { useDispatch, useSelector } from "react-redux";
+import { useState, useRef,Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { IoClose } from "react-icons/io5";
 
 function Layout() {
-  const user = "";
+  const user = {
+    name: "rajeev",
+    id: 1,
+    email: "rajeev@123",
+    passwd: "1234",
+  };
   const location = useLocation();
   return user ? (
     <div className="w-full h-screen flex flex-col md:flex-row">
       <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
-        {/* <Sidebar /> */}
+        <Sidebar />
       </div>
-        {/* <MobileSidebar /> */}
+      <MobileSidebar />
 
-        <div className="flex-1 overflow-y-auto">
-          {/* <Navbar /> */}
-          <div className="p-4 2xl:px-10">
-            <Outlet />
-          </div>
+      <div className="flex-1 overflow-y-auto">
+        <Navbar />
+        <div className="p-4 2xl:px-10">
+          <Outlet />
         </div>
+      </div>
     </div>
   ) : (
     <Navigate to={"/login-page"} state={{ from: location }} replace />
   );
 }
+
+const MobileSidebar = () => {
+  const { isSidebarOpen } = useSelector((state) => state.auth);
+  const mobileMenuRef = useRef(null);
+  const dispatch = useDispatch();
+
+  const closeSidebar = () => {
+    dispatch(setOpenSidebar(false));
+  };
+
+  return (
+    <>
+      <Transition
+        show={isSidebarOpen}
+        as={Fragment}
+        enter="transition-opacity duration-700"
+        enterFrom="opacity-x-10"
+        enterTo="opacity-x-100"
+        leave="transition-opacity duration-700"
+        leaveFrom="opacity-x-100"
+        leaveTo="opacity-x-0"
+      >
+        {(ref) => (
+          <div
+            ref={(node) => (mobileMenuRef.current = node)}
+            className={clsx(
+              "md:hidden w-full h-full bg-black/40 transition-all duration-700 transform ",
+              isSidebarOpen ? "translate-x-0" : "translate-x-full"
+            )}
+            onClick={() => closeSidebar()}
+          >
+            <div className="bg-white w-3/4 h-full">
+              <div className="w-full flex justify-end px-5 mt-5">
+                <button
+                  onClick={() => closeSidebar()}
+                  className="flex justify-end items-end"
+                >
+                  <IoClose size={25} />
+                </button>
+              </div>
+
+              <div className="-mt-10">
+                <Sidebar />
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+    </>
+  );
+};
 
 function App() {
   return (
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,1 +1,8 @@
-console.log('object')
+export function getInitials (fullName) {
+    const names = fullName?.split(' ')
+    console.log(names)
+    const initials = names?.slice(0,2)?.map(name => name[0].toUpperCase)
+    console.log(initials)
+    const str = initials?.join('')
+    return str
+}
